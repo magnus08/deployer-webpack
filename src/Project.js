d7d5efebf9fc4b7eaf17447951cfaa8395e9744e
Magnus Andersson <magnus.andersson@learnifier.com>
2017-07-08 23:27:27 +0200

    Toggler is now stateless.

--- src/Project.js
+++ src/Project.js
@@ -1,45 +1,62 @@
 import React from 'react';
 import Toggler from './Toggler';
 class Project extends React.Component {
-    handleToggleAutodeploy = () => (
-        this.props.onToggleAutoDeploy(this.props.id)
-    );
-    handleRedeploy = () => (
-        this.props.onRedeploy(this.props.id)
-    );
-    handleRebuild = () => (
-        this.props.onRebuild(this.props.id)
-    );
+  state = {
+    on: false,
+  };
 
-    render() {
-        return (
-            <tr>
-              <td className='center aligned'>
-                  <Toggler onClass="toggle on icon" offClass="toggle off icon"/>
-              </td>
-                <td className='center aligned'>
-                    <a onClick={this.handleToggleAutodeploy}>
-                        <i className= { this.props.autoDeploy?'toggle on icon':'toggle off icon' } />
-                    </a>
-                </td>
-                <td className='left aligned'>
-                    <p>
-                        {this.props.title}
-                    </p>
-                </td>
-                <td className='center aligned'>
-                    <a onClick={this.handleRedeploy}>
-                        <i className= { this.props.redeploying?'hourglass half icon':'refresh icon' } />
-                    </a>
-                </td>
-                <td className='center aligned'>
-                    <a onClick={this.handleRebuild}>
-                        <i className= { this.props.rebuilding?'hourglass half icon':'recycle icon' } />
-                    </a>
-                </td>
-            </tr>
-        );
-    }
+  on = () => {
+    this.setState(prevState => {
+      return {
+        on: !prevState.on,
+      }
+    });
+  };
+
+  handleToggleAutodeploy = () => (
+    this.props.onToggleAutoDeploy(this.props.id)
+  );
+  handleRedeploy = () => (
+    this.props.onRedeploy(this.props.id)
+  );
+  handleRebuild = () => (
+    this.props.onRebuild(this.props.id)
+  );
+
+  render() {
+    return (
+      <tr>
+        <td className='center aligned'>
+          <Toggler
+            on={this.state.on}
+            onClass="toggle on icon"
+            offClass="toggle off icon"
+            onClick={this.on}
+          />
+        </td>
+        <td className='center aligned'>
+          <a onClick={this.handleToggleAutodeploy}>
+            <i className= { this.props.autoDeploy?'toggle on icon':'toggle off icon' } />
+          </a>
+        </td>
+        <td className='left aligned'>
+          <p>
+            {this.props.title}
+          </p>
+        </td>
+        <td className='center aligned'>
+          <a onClick={this.handleRedeploy}>
+            <i className= { this.props.redeploying?'hourglass half icon':'refresh icon' } />
+          </a>
+        </td>
+        <td className='center aligned'>
+          <a onClick={this.handleRebuild}>
+            <i className= { this.props.rebuilding?'hourglass half icon':'recycle icon' } />
+          </a>
+        </td>
+      </tr>
+    );
+  }
 }
 
 export default Project;
--- src/Toggler.js
+++ src/Toggler.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-class Toggler extends React.Component {
-  state = {
-    on: false
-  };
-
-  toggle = (evt) => {
-    this.setState({
-      on: !this.state.on
-    })
-
-  }
-
-  static propTypes = {
-    onClass: PropTypes.string,
-    offClass: PropTypes.string
-  };
-
-  render() {
-    return (
-      <a onClick={this.toggle}>
-        <i className= { this.state.on?this.props.onClass:this.props.offClass } />
-      </a>
-    );
-  }
-}
+const Toggler = function (props) {
+  return (
+    <a onClick={props.onClick}>
+      <i className= {props.on?props.onClass:props.offClass} />
+    </a>
+  );
+};
 
 export default Toggler;
