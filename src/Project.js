7a64d4bbb32281d351e9fb2c51cc7c5eb30ecb03
Magnus Andersson <magnus.andersson@learnifier.com>
2017-07-11 00:21:30 +0200

    Formatting.

--- src/Project.js
+++ src/Project.js
@@ -50,38 +50,34 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   // TODO: I have no clue why i need to use bindActionCreators here, it should just work with thunks
   return bindActionCreators({
-    actionAutoDeploy: (id) => (
-      {
-        type: 'TOGGLE_PROJECT_AUTODEPLOY',
-        id: id
-      }
-    ),
+    actionAutoDeploy: (id) => ({
+      type: 'TOGGLE_PROJECT_AUTODEPLOY',
+      id: id
+    }),
     actionRedeploy: (id) => (dispatch) => {
       dispatch({
         type: 'TRIGGER_PROJECT_REDEPLOY',
         id: id
       });
-      return setTimeout(() => dispatch(
-        {
-          type: 'PROJECT_DEPLOY_DONE',
-          id: id
-        }), 2000)
-      },
-      actionRebuild: (id) => (dispatch) => {
-        dispatch({
-          type: 'TRIGGER_PROJECT_REBUILD',
-          id: id
-        });
-        return setTimeout(() => dispatch(
-          {
-            type: 'PROJECT_REBUILD_DONE',
-            id: id
-          }), 4000)
-        }
-      }, dispatch)
+      return setTimeout(() => dispatch({
+        type: 'PROJECT_DEPLOY_DONE',
+        id: id
+      }), 2000)
+    },
+    actionRebuild: (id) => (dispatch) => {
+      dispatch({
+        type: 'TRIGGER_PROJECT_REBUILD',
+        id: id
+      });
+      return setTimeout(() => dispatch({
+        type: 'PROJECT_REBUILD_DONE',
+        id: id
+      }), 4000);
     }
+  }, dispatch)
+};
 
-    export default connect(
-      mapStateToProps,
-      mapDispatchToProps
-    )(Project);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Project);
